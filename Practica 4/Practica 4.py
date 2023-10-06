b963a6fed120c013722d7df69d5cbdc65205bbc8
Alan-Santiago226 <72393801+Alan-Santiago226@users.noreply.github.com>
2023-10-06 02:54:35 -0600

    Update Sales_Genres Practica 4.py
    Se añadieron los plots para el segundo archivo .csv

--- Practica 4/Practica 4.py
+++ Practica 4/Practica 4.py
@@ -28,6 +28,28 @@ def Ventas(file_name: str, opcion: int) -> None:
     pyp.bar(df['Years'], df['Total_Sales'])
     pyp.savefig("videogamesales/test.png")
     pyp.close()
-    
+
+def Generos_Ventas(file_name: str) -> None:
+    df = pd.read_csv(file_name)
+    df_por_genero = df.groupby(["Genre", "Year"])[["MAX_Sales"]].aggregate(pd.DataFrame.mean)
+    df_por_genero.reset_index(inplace=True)
+    df_por_genero.set_index("Year", inplace=True)
+
+    for gen in set(df_por_genero["Genre"]):
+        plot_by_gen(df_por_genero, gen)
+
+    df_aux = df.groupby(["Genre", "Year"])[['MAX_Sales']].mean().unstack()
+    df_aux.plot(y = 'MAX_Sales', legend = False, figsize=(32,18))
+    pyp.xticks(rotation=90)
+    pyp.savefig("videogamesales/test-gen.png")
+    pyp.close()
+
+
+def plot_by_gen(df: pd.DataFrame, gen:str)->None:
+    df[df["Genre"] == gen].plot(y = ["MAX_Sales"], figsize=(12,8))
+    pyp.savefig(f"videogamesales/lt_{gen}.png")
+    df[df["Genre"] == gen].boxplot(by = 'Genre', figsize=(12,8))
+    pyp.savefig(f"videogamesales/bplt_{gen}.png")   
 
 Ventas(Top, 1)
+Generos_Ventas(Generos)
