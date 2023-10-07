d75eba0e2c3383af04f1a3d444a08fbea70e0cf3
Alan-Santiago226 <72393801+Alan-Santiago226@users.noreply.github.com>
2023-10-07 11:25:30 -0600

    Update Final Publisher Practica 4.py
    Se añadió la última función que añade plots a las mejores compañías por ventas y una en general (aunque incomprensible) del tercer archivo .csv

--- Practica 4/Practica 4.py
+++ Practica 4/Practica 4.py
@@ -1,11 +1,14 @@
-import numpy as np
 import pandas as pd
 from matplotlib import pyplot as pyp
+from itertools import cycle
 
 #Leemos los csv de la practica anterior (3)
 Top='videogamesales/vgsales_BestGames.csv'
 Generos = 'videogamesales/vgsales_BestGenres.csv'
 Compañias = 'videogamesales/vgsales_DS.csv'
+
+ls = cycle(['-','--',':','-.','-','--',':','-.','-','--',':','-.','-','--',':','-.','-','--',':','-.','-','--',':','-.'])
+#linecycler = cycle(ls)
 
 #print(df_1.head)
 #print(df_2.head)
@@ -28,7 +31,7 @@ def Ventas(file_name: str, opcion: int) -> None:
     pyp.bar(df['Years'], df['Total_Sales'])
     pyp.savefig("videogamesales/test.png")
     pyp.close()
-
+    
 def Generos_Ventas(file_name: str) -> None:
     df = pd.read_csv(file_name)
     df_por_genero = df.groupby(["Genre", "Year"])[["MAX_Sales"]].aggregate(pd.DataFrame.mean)
@@ -37,9 +40,10 @@ def Generos_Ventas(file_name: str) -> None:
 
     for gen in set(df_por_genero["Genre"]):
         plot_by_gen(df_por_genero, gen)
+        pyp.close()
 
-    df_aux = df.groupby(["Genre", "Year"])[['MAX_Sales']].mean().unstack()
-    df_aux.plot(y = 'MAX_Sales', legend = False, figsize=(32,18))
+    df_aux = df.groupby(["Year", "Genre"])[['MAX_Sales']].mean().unstack()
+    df_aux.plot(y = 'MAX_Sales', legend = True,figsize=(32,18))
     pyp.xticks(rotation=90)
     pyp.savefig("videogamesales/test-gen.png")
     pyp.close()
@@ -51,5 +55,43 @@ def plot_by_gen(df: pd.DataFrame, gen:str)->None:
     df[df["Genre"] == gen].boxplot(by = 'Genre', figsize=(12,8))
     pyp.savefig(f"videogamesales/bplt_{gen}.png")   
 
+
+def Mercado(file_name: str) -> None:
+    df = pd.read_csv(file_name)
+    df_aux = df.groupby('Year')['MAX_Sales'].idxmax().apply(lambda x: df['Publisher'].iloc[x])
+    df_Pp = df.groupby(["Publisher", "Year"])[["MAX_Sales"]].aggregate(pd.DataFrame.mean)
+    df_Pp.reset_index(inplace=True)
+    df_Pp.set_index("Publisher", inplace=False)
+    Pub_aux = df_Pp['Publisher'].unique()
+    #print(df_aux)
+
+    for Pub in df_aux:
+        df_publisher = df[df['Publisher'] == Pub]
+        pyp.figure(figsize=(32,18))
+        pyp.plot(df_publisher['Year'],df_publisher['MAX_Sales'], label = Pub, linewidth = 5)
+        pyp.xlabel('Years') 
+        pyp.ylabel('Million Sales')
+        pyp.title(f'Million Sales by {Pub} each Year')
+        pyp.legend()
+        pyp.savefig(f"videogamesales/SY_{Pub}.png")
+        pyp.close()
+    
+    pyp.figure(figsize=(64,36))
+    for Pub in Pub_aux:
+        style = next(ls)
+        df_publisher = df_Pp[df_Pp['Publisher'] == Pub]
+        pyp.plot(df_publisher['Year'], df_publisher['MAX_Sales'], label = Pub, linewidth = 2, linestyle = style)
+
+    pyp.xlabel('Years') 
+    pyp.ylabel('Million Sales')
+    pyp.title('Million Sales each Year')
+    pyp.legend()
+    pyp.savefig("videogamesales/SY_General.png")
+    pyp.close()
+
+
+
 Ventas(Top, 1)
 Generos_Ventas(Generos)
+Mercado(Compañias)
+ 
